25d628bdbd4a631ac2f05144c1ca1fba22bb8c17
Nitin <nmohan101@gmail.com>
2018-08-10 19:06:38 -0400

    Added tools

---
+++ Tools/tools.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+"""tools.sh: Helper script
+
+__author__      = "Nitin Mohan
+__copyright__   = "Copy Right 2018. NM Technlogies"
+"""
+
+echo Mounting Drive
+nmohan@192.168.1.31:/home/nitin/Documents/Photo_Network  /home/nmohan/Documents/Photo_Network
