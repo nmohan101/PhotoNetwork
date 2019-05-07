5c1e0d8d93d777e213d9fa27cc6e19fbb99a0dc2
codemonkey111 <rentals.tdot@gmail.com>
2019-05-07 10:46:54 -0400

    Add existing file

--- FrontEnd/myFirstLinuxGit.py
+++ FrontEnd/myFirstLinuxGit.py
@@ -1,5 +1,1 @@
-This is a vim test file.  If you are reading this then you are probably in denial about your sexuality, drive a Jeep and carry a joke-book.
-
-Linux biatch!
-
-Now how do I save this bitch...
+This is an edit to my first file.  Lets see if I got this upload/commit/push stuff down... 
