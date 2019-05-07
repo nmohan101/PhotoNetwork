0bdab1c7b2b0bf1da5d1dd483af727f0408c2084
codemonkey111 <rentals.tdot@gmail.com>
2019-05-07 10:13:15 -0400

    Add existing file

--- FrontEnd/database/tables.py
+++
@@ -1,7 +0,0 @@
-import django_tables2 as tables
-from .models import IncomingData
-
-class IncomingDataTable(tables.Table):
-    class Meta:
-        model = IncomingData
-        template_name = 'django_tables2/bootstrap.html'
---
+++ FrontEnd/myFirstLinuxGit.py
@@ -0,0 +1,5 @@
+This is a vim test file.  If you are reading this then you are probably in denial about your sexuality, drive a Jeep and carry a joke-book.
+
+Linux biatch!
+
+Now how do I save this bitch...
