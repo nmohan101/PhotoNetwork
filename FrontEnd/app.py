f3a5d4a509982e81ab372df02c3d1dc5ce782580
codemonkey111 <rentals.tdot@gmail.com>
2019-05-14 17:53:59 -0400

    I broke a few things after the last commit.  They seem to be fixed.  This is still just the display of the db file

--- FrontEnd/app.py
+++ FrontEnd/app.py
@@ -23,4 +23,5 @@ def hello():
 
 app.jinja_env.auto_reload = True
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.run(debug=True, host='127.0.0.1')
+if __name__ == "__main__":
+    app.run(debug=True, host='127.0.0.1')
