fd38c817e0fc4b1b14424e3f1c57fbdb60540ebf
nmohan101 <nmohan101@gmail.com>
2019-06-22 22:26:02 -0400

    Updated pn_controller, start and stop works

--- bin/pn_controller.h
+++ bin/pn_controller.h
@@ -7,6 +7,9 @@ __copyright__   = "Copy Right 2018. NM Technologies"
 /                   System Imports                  /
 /--------------------------------------------------*/
 #include <stdbool.h>
+#include <stdio.h>
+#include <getopt.h>
+#include <unistd.h>
 
 /*--------------------------------------------------/
 /                   Constants                       /
@@ -14,16 +17,29 @@ __copyright__   = "Copy Right 2018. NM Technologies"
 #define SETTINGS_PATH "/etc/PhotoNetwork/network_settings.json"
 
 /*---------Structures-------------*/
+struct option long_options[] = 
+{
+    {"start", no_argument, 0, 's'},
+    {"stop",  no_argument, 0, 'k'},
+    {"help",  no_argument, 0, 'h'}
+};
+
 struct network_settings
 {
     const char *network_mode;
-    bool sim_mode;
     const char *test_path;
     const char *app_path;
-    const char apps[100][5];
     const char *py_ext;
+    size_t     n_of_apps;
+    bool       sim_mode;
+    char       apps[][100];
 };
 
 //----------- Prototype ----------//
+static void print_usage(int es);
 static void read_file(char *filepath, char *file_content);
 static void parse_json(char *file_content, struct network_settings *object_p);
+static void start_pn(struct network_settings *object_p);
+static void stop_pn();
+
+//-----end of file--------------//
